8cf01ebf1c0134b3b611e9ff8cf49a8272db0661
jaspreet singh <jaspreet1731.be21@chitkara.edu.in>
2023-12-02 15:14:33 +0600

    commit

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,70 +1,94 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DELIVERED_FAIL, ORDER_DELIVERED_REQUEST, ORDER_DELIVERED_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_LIST_FAIL, ORDER_LIST_MY_FAIL, ORDER_LIST_MY_REQUEST, ORDER_LIST_MY_SUCCESS, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS } from "../constants/orderConstansts";
+import {
+  ORDER_CREATE_FAIL,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_DELIVERED_FAIL,
+  ORDER_DELIVERED_REQUEST,
+  ORDER_DELIVERED_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_LIST_FAIL,
+  ORDER_LIST_MY_FAIL,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_REQUEST,
+  ORDER_LIST_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+} from "../constants/orderConstansts";
 import axios from "axios";
 
-export const createOrder =(order)=>async(dispatch,getState)=>{
-    try{
-        dispatch({
-            type:ORDER_CREATE_REQUEST
-        })
-        const {userLogin:{userInfo}}=getState()
-        const config={
-            headers:{
-                'Content-type':'application/json',
-                Authorization:`Bearer ${userInfo.token}`
+export const createOrder = (order) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_CREATE_REQUEST,
+    });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.post(
+      `https://gg-6702.onrender.com/api/orders`,
+      order,
+      config
+    );
+    dispatch({
+      type: ORDER_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-            }
-        }
-        const {data}=await axios.post(`/api/orders`,order,config)
-        dispatch({
-            type:ORDER_CREATE_SUCCESS,
-            payload:data
-        })
-       
-    }catch(error){
-        dispatch({
-            type:ORDER_CREATE_FAIL,
-            payload:
-            error.response && error.response.data.message?
-            error.response.data.message:
-            error.message,
-        })
-    }
-}
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  console.log("is");
+  try {
+    dispatch({
+      type: ORDER_DETAILS_REQUEST,
+    });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.get(
+      `https://gg-6702.onrender.com/api/orders/${id}`,
+      config
+    );
+    console.log("this is data ");
+    console.log(data);
 
-export const getOrderDetails =(id)=>async(dispatch,getState)=>{
-    console.log('is')
-    try{
-        dispatch({
-            type:ORDER_DETAILS_REQUEST
-        })
-        const {userLogin:{userInfo},}=getState()
-        const config={
-            headers:{
-               
-                Authorization:`Bearer ${userInfo.token}`,
-
-            },
-        }
-        const {data}=await axios.get(`/api/orders/${id}`,config)
-        console.log('this is data ')
-      console.log(data)
-       
-        dispatch({
-            type:ORDER_DETAILS_SUCCESS,
-            payload:data    
-        })
-       
-    }catch(error){
-        dispatch({
-            type:ORDER_DETAILS_FAIL,
-            payload:
-            error.response && error.response.data.message?
-            error.response.data.message:
-            error.message,
-        })
-    }
-}
-
+    dispatch({
+      type: ORDER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 // export const getOrderDetails = (orderId) => async (dispatch, getState) => {
 //     try {
@@ -80,143 +104,151 @@ export const getOrderDetails =(id)=>async(dispatch,getState)=>{
 //     }
 //   }
 
+export const payOrder =
+  (orderId, paymentResult) => async (dispatch, getState) => {
+    console.log("is");
+    try {
+      dispatch({
+        type: ORDER_PAY_REQUEST,
+      });
+      const {
+        userLogin: { userInfo },
+      } = getState();
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+      const { data } = await axios.put(
+        `https://gg-6702.onrender.com/api/orders/${orderId}/pay`,
+        paymentResult,
+        config
+      );
+      console.log("this is data ");
+      console.log(data);
 
-export const payOrder =(orderId,paymentResult)=>async(dispatch,getState)=>{
-    console.log('is')
-    try{
-        dispatch({
-            type:ORDER_PAY_REQUEST
-        })
-        const {userLogin:{userInfo},}=getState()
-        const config={
-            headers:{
-               'Content-Type':'application/json',
-                Authorization:`Bearer ${userInfo.token}`,
-
-            },
-        }
-        const {data}=await axios.put(`/api/orders/${orderId}/pay`,paymentResult,config)
-        console.log('this is data ')
-      console.log(data)
-       
-        dispatch({
-            type:ORDER_PAY_SUCCESS,
-            payload:data    
-        })
-       
-    }catch(error){
-        dispatch({
-            type:ORDER_PAY_FAIL,
-            payload:
-            error.response && error.response.data.message?
-            error.response.data.message:
-            error.message,
-        })
+      dispatch({
+        type: ORDER_PAY_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: ORDER_PAY_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
     }
-}
+  };
 
+export const listMyOrders = () => async (dispatch, getState) => {
+  // console.log('is')
+  try {
+    dispatch({
+      type: ORDER_LIST_MY_REQUEST,
+    });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.get(
+      `https://gg-6702.onrender.com/api/orders/myorders`,
+      config
+    );
+    console.log("this is data ");
+    console.log(data);
 
-export const listMyOrders =()=>async(dispatch,getState)=>{
-    // console.log('is')
-    try{
-        dispatch({
-            type:ORDER_LIST_MY_REQUEST
-        })
-        const {userLogin:{userInfo},}=getState()
-        const config={
-            headers:{
-                Authorization:`Bearer ${userInfo.token}`,
+    dispatch({
+      type: ORDER_LIST_MY_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_MY_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-            },
-        }
-        const {data}=await axios.get(`/api/orders/myorders`,config)
-        console.log('this is data ')
-      console.log(data)
-       
-        dispatch({
-            type:ORDER_LIST_MY_SUCCESS,
-            payload:data    
-        })
-       
-    }catch(error){
-        dispatch({
-            type:ORDER_LIST_MY_FAIL,
-            payload:
-            error.response && error.response.data.message?
-            error.response.data.message:
-            error.message,
-        })
-    }
-}
+export const listOrders = () => async (dispatch, getState) => {
+  // console.log('is')
+  try {
+    dispatch({
+      type: ORDER_LIST_REQUEST,
+    });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.get(
+      `https://gg-6702.onrender.com/api/orders`,
+      config
+    );
+    console.log("this is data ");
+    console.log(data);
 
- 
+    dispatch({
+      type: ORDER_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
+export const deliverOrder = (order) => async (dispatch, getState) => {
+  console.log("after actions");
+  console.log(order._id);
+  try {
+    dispatch({
+      type: ORDER_DELIVERED_REQUEST,
+    });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.put(
+      `https://gg-6702.onrender.com/api/orders/${order._id}/deliver`,
+      {},
+      config
+    );
+    console.log("this is data ");
+    console.log(data);
 
-
-export const listOrders =()=>async(dispatch,getState)=>{
-    // console.log('is')
-    try{
-        dispatch({
-            type:ORDER_LIST_REQUEST
-        })
-        const {userLogin:{userInfo},}=getState()
-        const config={
-            headers:{
-                Authorization:`Bearer ${userInfo.token}`,
-
-            },
-        }
-        const {data}=await axios.get(`/api/orders`,config)
-        console.log('this is data ')
-      console.log(data)
-       
-        dispatch({
-            type:ORDER_LIST_SUCCESS,
-            payload:data    
-        })
-       
-    }catch(error){
-        dispatch({
-            type:ORDER_LIST_FAIL,
-            payload:
-            error.response && error.response.data.message?
-            error.response.data.message:
-            error.message,
-        })
-    }
-}
-
- 
-export const deliverOrder =(order)=>async(dispatch,getState)=>{
-    console.log('after actions')
-    console.log(order._id)
-    try{
-        dispatch({
-            type:ORDER_DELIVERED_REQUEST
-        })
-        const {userLogin:{userInfo},}=getState()
-        const config={
-            headers:{
-                Authorization:`Bearer ${userInfo.token}`,
-
-            },
-        }
-        const {data}=await axios.put(`/api/orders/${order._id}/deliver`,{},config)
-        console.log('this is data ')
-      console.log(data)
-       
-        dispatch({
-            type:ORDER_DELIVERED_SUCCESS,
-            payload:data    
-        })
-       
-    }catch(error){
-        dispatch({
-            type:ORDER_DELIVERED_FAIL,
-            payload:
-            error.response && error.response.data.message?
-            error.response.data.message:
-            error.message,
-        })
-    }
-}
+    dispatch({
+      type: ORDER_DELIVERED_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_DELIVERED_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,36 +1,33 @@
-import React from 'react'
-import { Card } from 'react-bootstrap' 
-import {Link } from 'react-router-dom'
-import Rating from './Rating'
-const Product = ({product}) => {
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
-     
-        {/* <h1>{product._id}</h1> */}
-        <Link to={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant='top'/>
-        </Link>
-      
-    <Card.Body>
-        <Link to={`/product/${product._id}`}>
-            <Card.Title as='div'>
-                <strong>{product.name}</strong>
-            </Card.Title>
-        </Link>
-    </Card.Body>
+    <Card className="my-3 p-3 rounded">
+      {/* <h1>{product._id}</h1> */}
+      <Link to={`https://gg-6702.onrender.com/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </Link>
 
-    <Card.Text ad='div'>
-        <Rating >
-            value={product.rating} text={`{product.numReviews} reviews`}
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+      </Card.Body>
+
+      <Card.Text ad="div">
+        <Rating>
+          value={product.rating} text={`{product.numReviews} reviews`}
         </Rating>
-    </Card.Text>
+      </Card.Text>
 
-    <Card as='text'>
-        ${product.price}
+      <Card as="text">${product.price}</Card>
+      {/* <h1>{product._id}</h1> */}
     </Card>
-    {/* <h1>{product._id}</h1> */}
-    </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
